51d7ee150e1a0cb81ea450d44beafc5d3a887de7
annagiuliam <annagiulia.musso@gmail.com>
2026-03-23 22:06:23 +0100

    feat: added get mapping for all users

--- src/main/java/barker_server/adapter/in/web/UserController.java
+++ src/main/java/barker_server/adapter/in/web/UserController.java
@@ -3,7 +3,7 @@ package barker_server.adapter.in.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import barker_server.domain.in.RegisterUserUseCase;
+import barker_server.domain.in.UserUseCase;
 import barker_server.domain.model.user.User;
 import barker_server.exception.UserNotFoundException;
 import jakarta.validation.Valid;
@@ -16,29 +16,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-  private final RegisterUserUseCase registerUserUseCase;
+  private final UserUseCase userUseCase;
 
-  public UserController(RegisterUserUseCase registerUserUseCase) {
-    this.registerUserUseCase = registerUserUseCase;
+  public UserController(UserUseCase userUseCase) {
+    this.userUseCase = userUseCase;
   }
 
   private List<User> users = new ArrayList<>();
-
-  @GetMapping
-  public ResponseEntity<List<User>> getAllUsers() {
-    return ResponseEntity.ok(users);
-  }
 
   @PostMapping
   public ResponseEntity<Void> registerUser(@Valid @RequestBody RegisterUserRequest registeredUser) {
     // call the interface because Spring injects a concrete object that implements
     // it (RegisterUserService)
-    registerUserUseCase.register(registeredUser.username(), registeredUser.password());
+    userUseCase.register(registeredUser.username(), registeredUser.password());
 
     return ResponseEntity.status(201).build();
   }
@@ -50,6 +46,11 @@ public class UserController {
         .findFirst()
         .map(ResponseEntity::ok)
         .orElseThrow(() -> new UserNotFoundException(id));
+  }
+
+  @GetMapping("")
+  public ResponseEntity<List<User>> getAllUsers() {
+    return ResponseEntity.ok(userUseCase.getAllUsers());
   }
 
 }
