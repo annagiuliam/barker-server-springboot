153fa399672abaf655efcbed70e1f0bc79b1e927
annagiuliam <annagiulia.musso@gmail.com>
2026-04-05 14:26:26 +0200

    feat: add validation for username length for register user request

--- src/main/java/barker_server/adapter/in/web/RegisterUserRequest.java
+++ src/main/java/barker_server/adapter/in/web/RegisterUserRequest.java
@@ -3,5 +3,8 @@ package barker_server.adapter.in.web;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record RegisterUserRequest(@NotBlank String username, @NotBlank @Size(min = 8) String password) {
+public record RegisterUserRequest(
+    @NotBlank @Size(min = 3, max = 20, message = "Username must be 3-20 characters long") String username,
+
+    @NotBlank @Size(min = 8) String password) {
 }
